71e42e40186ed2e33a0c4e0e8577543ec73cbf66
shagu <ericmauser@googlemail.com>
2021-11-03 22:27:48 +0100

    conf: add love2d config

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.title = "MTGCounter"
+    t.window.icon = nil
+    t.window.fullscreen = true
+    t.window.resizable = true
+    t.window.x = 0
+    t.window.y = 0
+end
